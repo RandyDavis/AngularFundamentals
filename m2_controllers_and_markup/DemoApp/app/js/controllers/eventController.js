c789ebb0eb026d02943cf6f9e377fe4ca3b83ae0
Randy Davis <randy_davis@outlook.com>
2016-05-16 01:05:11 -0500

    add event controller with initial event data; add bootstrap 3 and add event details page

---
+++ m2_controllers_and_markup/DemoApp/app/js/controllers/eventController.js
@@ -0,0 +1,21 @@
+/**
+ * Created by randy on 5/16/16.
+ */
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                state: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png'
+        }
+    }
+);
