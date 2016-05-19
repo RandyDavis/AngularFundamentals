1d86f184b681f82c0336780a1df52760c61fa6da
Randy Davis <randy_davis@outlook.com>
2016-05-18 22:58:05 -0500

    complete handling events demo; adds the upvote session functionality

--- m2_controllers_and_markup/DemoApp/app/js/controllers/eventController.js
+++ m2_controllers_and_markup/DemoApp/app/js/controllers/eventController.js
@@ -22,23 +22,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do ir for you'
+                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do ir for you',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
                     duration: '2 hr',
                     level: 'Intermediate',
-                    abstract: 'Controllers are the beginning of everything Angular does.'
+                    abstract: 'Controllers are the beginning of everything Angular does.',
+                    upVoteCount: 0
                 }
             ]
+        }
+
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        }
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
         }
     }
 );
