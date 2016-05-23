565adb475585cbb252e0b14a645b93a50e25f18d
Randy Davis <randy_davis@outlook.com>
2016-05-23 00:19:12 -0500

    complete creating your first custom service section

--- m2_controllers_and_markup/DemoApp/app/js/controllers/eventController.js
+++ m2_controllers_and_markup/DemoApp/app/js/controllers/eventController.js
@@ -4,51 +4,15 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope, eventData) {
 
         $scope.myStyle = {color: 'red'};
         $scope.snippet = '<span style="color: red">hi there</span>';
         $scope.boolValue = false;
         $scope.myClass = "blue";
         $scope.buttonDisabled = true;
-        $scope.sortOrder = "name",
-        $scope.event = {
-            name: 'Angular Boot Camp',
-            date: 1359781015626,
-            time: '10:30am',
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                state: 'CA'
-            },
-            imageUrl: '/img/angularjs-logo.png',
-            sessions: [
-                {
-                    name: 'Directives Masterclass',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!',
-                    upVoteCount: 4
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'John Doe',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do ir for you',
-                    upVoteCount: 2
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Jane Doe',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Controllers are the beginning of everything Angular does.',
-                    upVoteCount: 8
-                }
-            ]
-        }
+        $scope.sortOrder = "name";
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
