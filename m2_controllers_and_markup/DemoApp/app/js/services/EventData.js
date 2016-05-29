86d7e37ee98d9fcf9384cdfc6c39941a29f6a137
Randy Davis <randy_davis@outlook.com>
2016-05-29 02:04:14 -0500

    complete the updateing a node server for JSON requests section

--- m2_controllers_and_markup/DemoApp/app/js/services/EventData.js
+++ m2_controllers_and_markup/DemoApp/app/js/services/EventData.js
@@ -3,10 +3,10 @@
  */
 eventsApp.factory('eventData', function ($http, $log) {
     return {
-         getEvent: function (success) {
+         getEvent: function (successcb) {
             $http({method: 'GET', url: '/data/event/1'})
                 .success(function (data, status, headers, config) {
-                    success(data);
+                    successcb(data);
                 })
                 .error(function (data, status, headers, config) {
                     $log.warn(data, status, headers, config);
